76bd380c4987d1bcbd50b4fc38d0201958c24c03
mahdihma <mahdi.haji@yahoo.com>
2020-08-01 15:20:45 +0430

    tokenizer class added

---
+++ src/app/Tokenizer.java
@@ -0,0 +1,14 @@
+package app;
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Tokenizer {
+    private HashMap<String, Integer> invertedIndex;
+    private ArrayList<String> filePaths;
+    public Tokenizer(ArrayList<String> filePaths) {
+        this.filePaths = filePaths;
+    }
+
+    public void tokenize(){
+
+    }
+}
