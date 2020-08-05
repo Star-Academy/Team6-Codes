49752524596b2a115e6013e5d670c1c4951960dd
mahdihma <mahdi.haji@yahoo.com>
2020-08-05 15:08:45 +0430

    QueryTest added

--- src/test/QueryTest.java
+++ src/test/QueryTest.java
@@ -1,6 +1,36 @@
 package test;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import app.Query;
+import app.Tokenizer;
+
 public class QueryTest {
-    
-    
+    private Query query;
+    private HashMap<String, HashSet<Integer>> invertedIndex;
+    private String folderPath = "\\src\\test\\test-data";
+
+    @Before
+    public void setInvertedIndex() {
+        System.out.println(new File(folderPath).getAbsolutePath());
+        Tokenizer tokenizer = new Tokenizer(folderPath);
+
+        invertedIndex = tokenizer.getInvertedIndex();
+    }
+
+    @Test
+    public void testQuery() {
+        String queryStr = "+slm -mahdi mohamadhosein";
+        query = new Query(queryStr, invertedIndex);
+        HashSet<Integer> result = query.process();
+        HashSet<Integer> expectedResult = new HashSet<>();
+        expectedResult.add(3);
+        Assert.assertEquals(expectedResult, result);
+    }
 }
