03e9afe6299092c848efa7cec4ca253e2323ea1a
mahdihma <mahdi.haji@yahoo.com>
2020-08-09 13:42:02 +0430

    inverted index dependency improved!

--- src/test/QueryTest.java
+++ src/test/QueryTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -13,19 +14,22 @@ import app.Tokenizer;
 
 public class QueryTest {
     private Query query;
-    private HashMap<String, HashSet<Integer>> invertedIndex;
+    private HashMap<String, HashSet<Integer>> invertedIndex = new HashMap<>();
     private String folderPath = "/home/mahdi/Documents/code-star/Team-6/src/test/test-data";
 
     @Before
     public void setInvertedIndex() {
         System.out.println(new File(folderPath).getAbsolutePath());
-        Tokenizer tokenizer = new Tokenizer(folderPath);
-        invertedIndex = tokenizer.getInvertedIndex();
+        invertedIndex.put("slm", new HashSet<>(Arrays.asList(1, 2, 3)));
+        invertedIndex.put("mahdi", new HashSet<>(Arrays.asList(1, 2)));
+        invertedIndex.put("mohamadhosein", new HashSet<>(Arrays.asList(2, 3)));
+        invertedIndex.put("haj", new HashSet<>(Arrays.asList(1)));
+
     }
 
     @Test
     public void testQuery1() {
-        String queryStr = "+slm -mahdi mohamadhosein halet khobe";
+        String queryStr = "+slm -mahdi mohamadhosein";
         query = new Query(queryStr, invertedIndex);
         HashSet<Integer> result = query.process();
         HashSet<Integer> expectedResult = new HashSet<>();
