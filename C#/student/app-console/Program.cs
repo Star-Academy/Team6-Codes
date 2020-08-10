1b57ad83f3e5a1824143ca0d624772e73007c065
mohammadhheidarzade <dr.mhheidarzade@gmail.com>
2020-08-10 12:42:09 +0430

    init c# student  project

---
+++ C#/student/app-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
