ba10a1460649d7365d9f0ac8cb700aa0ec771ae0
mohammadhheidarzade <dr.mhheidarzade@gmail.com>
2020-08-10 16:51:22 +0430

    refactor student scores projects

--- C#/student/app-console/controller/Manager.cs
+++ C#/student/app-console/controller/Manager.cs
@@ -1,5 +1,5 @@
 
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using app_console.model;
 using app_console.view;
@@ -16,17 +16,17 @@ namespace app_console.controller
 
         public static void Run()
         {
-            Grade[] grades = new Deserializer<Grade[]>(scoresJsonFilePath).getObject();
-            Student[] students = new Deserializer<Student[]>(studentsJsonFilePath).getObject();
+            List<Grade> grades = new Deserializer<List<Grade>>(scoresJsonFilePath).getObject();
+            List<Student> students = new Deserializer<List<Student>>(studentsJsonFilePath).getObject();
             students = AssignGrades(students , grades);
-            students.ToList().ForEach(student => student.setAvarage());
-            students = students.OrderByDescending(student => student.avarage).ToArray();
+            students.ForEach(student => student.setAvarage());
+            students = students.OrderByDescending(student => student.avarage).ToList();
             View.ShowTop(students, topLength);
         }
 
-        public static Student[] AssignGrades(Student[] students , Grade[] grades){
-            students = students.OrderBy(student => student.StudentNumber).ToArray();
-            grades.ToList().ForEach(grade => students[grade.StudentNumber - 1].grades.Add(grade));
+        public static List<Student> AssignGrades(List<Student> students , List<Grade> grades){
+            students = students.OrderBy(student => student.StudentNumber).ToList();
+            grades.ForEach(grade => students[grade.StudentNumber - 1].grades.Add(grade));
             return students;
         }
 
