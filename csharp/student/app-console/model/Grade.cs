9c009f1709bf54a753a14411f3b15a96117fe734
mahdihma <mahdi.haji@yahoo.com>
2020-08-11 21:17:15 +0430

    refactor mistake

--- csharp/student/app-console/model/Grade.cs
+++
@@ -1,9 +0,0 @@
-namespace app_console.model
-{
-    public class Grade
-    {
-        public int StudentNumber{get; set;}
-        public string Lesson    {get; set;}
-        public float Score      {get; set;}
-    }
-}
