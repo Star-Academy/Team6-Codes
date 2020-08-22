5b906f8a39d362e73f0775c9358f6168d28c6c4d
mohammadhheidarzade <dr.mhheidarzade@gmail.com>
2020-08-22 14:39:06 +0430

    initial googleSearch with elasticsarch

---
+++ csharp/ElasticSearch/GoogleElasticsearch/GoogleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ csharp/ElasticSearch/GoogleElasticsearch/GoogleApp/Repository/FolderReader.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.IO;
+namespace GoogleApp.Repository
+{
+    public class FolderReader
+    {
+        private string folderPath;
+
+        public FolderReader(string folderPath){
+            this.folderPath = folderPath;
+        }
+
+        public List<string> getFilesPath(){
+            
+        }
+    }
+}
